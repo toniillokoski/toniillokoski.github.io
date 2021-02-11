b1d4d8f956ab91416b069a5c57620939b70625fd
Toni Illokoski <toni.illokoski@gmail.com>
2021-02-11 23:31:05 +0200

    new MPC app added

---
+++ mpc/mpc.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+    //Sounds
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        })
+    })
+});
